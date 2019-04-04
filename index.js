ac6be1a0dc9c3a0d0a933d076afe7023416ba56b
Tamás Németh <tomuwhu@gmail.com>
2019-04-04 13:29:36 +0200

    .

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-let express = require('express');
-let app = express();
-
-app.set('view engine', 'ejs');
-
+let express = require('express')
+let app = express()
+app.set('view engine', 'ejs')
+app.set('view options', { delimiter: '?' })
 let x = [3,4,5,2,3,5]
 
 app.get('/', (req, res) => {
-  res.render('index', { x } )
+  res.render('index', { x })
 })
 
-app.listen(4000, () => console.log('Example app listening on port 4000!'))
+app.listen(4000)
