1bf6bfd1d28a6dff0a398e1b4e303438c9f8ba46
Tamás Németh <tomuwhu@gmail.com>
2019-04-04 14:49:17 +0200

    .

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ let express = require('express')
 let app = express()
 app.set('view engine', 'ejs')
 app.set('view options', { delimiter: '?' })
-
-app.get('/', (req, res) => {
+let y = 0
+app.use('/', (req, res) => {
+  y++
   let x = Array( 90 )
             .fill( )
-            .map( (v, i) => i )
+            .map( (v, i) => i+1 )
             .sort( () => Math.random() - .5 )
             .slice( 0, 5 )
             .sort( (a, b) => a - b )
-  res.render( 'index', { x } )
+  res.render( 'index', { x, y } )
 })
 
 app.listen(4000)
